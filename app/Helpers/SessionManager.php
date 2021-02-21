bd64f9af0380d0722f0676f6478115204b2e0611
findambrose <ambkyusya@gmail.com>
2021-02-21 12:33:14 +0300

    Change the session manager

--- app/Helpers/SessionManager.php
+++ app/Helpers/SessionManager.php
@@ -2,10 +2,10 @@
 namespace App\Helpers;
 class SessionManager{
   public function sessionOn($menuResponse){
-   echo 'CON'.$menuResponse ;
+   echo 'CON '.$menuResponse ;
   }
   public function sessionEnded($menuResponse){
-    echo 'END'.$menuResponse ;
+    echo 'END '.$menuResponse ;
   }
 }
 ?>
